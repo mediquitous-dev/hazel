3221f87e65a8c0da90ecc49aa5bd6fa39f8f1f67
hckim <hckim@mediquitous.com>
2023-12-12 16:03:02 +0900

    remove print

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
 chrome.runtime.onMessage.addListener(function(event, sender, sendResponse) {
     if (event.type === 'requestAdminApi') {
-        console.log('requestAdminApi', event)
         chrome.tabs.query({url: 'https://admin.nugu.jp/*'}, function(tabs) {
-            console.log('tabs', tabs)
             chrome.tabs.sendMessage(tabs[0].id, event)
         })
     }
     if (event.type === 'responseAdminApi') {
-        console.log('responseAdminApi', event)
         chrome.tabs.query({url: 'https://www.nugu.jp/*'}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, event)
         })
