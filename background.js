5725deb4d96c78e3dc206605be80c68cb4ff0460
hckim <hckim@mediquitous.com>
2023-12-12 23:11:09 +0900

    어드민 안열렸을때 대응

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.runtime.onMessage.addListener(function(event, sender, sendResponse) {
     if (event.type === 'requestAdminApi') {
         chrome.tabs.query({url: 'https://admin.nugu.jp/*'}, function(tabs) {
+            if (tabs.length === 0) {
+                chrome.tabs.create({url: 'https://admin.nugu.jp/'}).then(function(tab) {
+                    chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+                        if (tabId === tab.id && changeInfo.status === 'complete') {
+                            chrome.tabs.onUpdated.removeListener(listener)
+                            chrome.tabs.sendMessage(tab.id, event)
+                        }
+                    })
+                })
+            }
+        })
+        chrome.tabs.query({url: 'https://admin.nugu.jp/*'}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, event)
         })
     }
